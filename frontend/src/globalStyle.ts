4227f444a7891652cfc778978f35898b18940bbb
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-11-02 15:22:38 -0300

    Adicionado GlobalStyle

--- frontend/src/globalStyle.ts
+++ frontend/src/globalStyle.ts
@@ -1,6 +1,10 @@
-* {
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+ * {
   margin: 0;
   padding: 0;
+  outline: 0;
   box-sizing: border-box;
 }
 
@@ -21,3 +25,4 @@ html, body {
   width: 100%;
   height: 100vh;
 }
+`;
