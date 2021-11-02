685865b7fd0f64444501902116d1cf6f795519ce
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-11-02 17:25:24 -0300

    Adicionado endpoint de retornar o usuário pelo uuid.

---
+++ backend/controllers/GetUserByUuidController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { DisableUserByEmailService } from '../services/DisableUserByEmailService'
+import { VerifyIfUserRegisteredService } from '../services/VerifyIfUserRegisteredService'
+
+class GetUserByUuidController {
+    async handle(request: Request, response: Response) {
+        const { uuid } = request.params
+        try {
+            const user = await new VerifyIfUserRegisteredService().execute(null, uuid)
+            response.status(200).json({ user })
+        } catch (e) {
+            console.log(e)
+            response.sendStatus(400)
+        }
+    }
+}
+
+export {
+    GetUserByUuidController
+}
--- backend/router.ts
+++ backend/router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { CreateUserAndVacineController } from './controllers/CreateUserAndVacineController'
 import { DisableUserByEmailController } from './controllers/DisableUserByEmailController'
+import { GetUserByUuidController } from './controllers/GetUserByUuidController'
 import { SendEmailService } from './services/SendEmailService'
 
 const router = Router()
 
 router.post("/api/v1/user/save", new CreateUserAndVacineController().handle)
 router.get("/api/v1/user/sav", new SendEmailService().execute)
+router.get("/api/v1/user/:uuid", new GetUserByUuidController().handle)
 router.get("/api/v1/user/disable/:uuid", new DisableUserByEmailController().handle)
-
 export default router
